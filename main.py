f673172eea76b0f25b2ff001e06ef986a879bf9b
Joshua Snider <josh@joshuasnider.com>
2016-02-12 13:07:06 -0500

    Placeholders for new files.

--- main.py
+++ main.py
@@ -10,17 +10,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    """TODO"""
-    return render_template('home.html',
-        img_url='http://www.jpl.nasa.gov/images/kepler/20120111/pia15257-640.jpg')
+  """Show the homepage"""
+  return render_template('home.html',
+    img_url='http://www.jpl.nasa.gov/images/kepler/20120111/pia15257-640.jpg')
+
+@app.route('/planet/<planet>')
+def planet_page(planet):
+  return render_template('planet.html',
+    img_url='http://www.jpl.nasa.gov/images/kepler/20120111/pia15257-640.jpg',
+    planet_name=planet)
+
+@app.route('/system/<sysname>')
+def system_page(sysname):
+  return render_template('system.html',
+    img_url='http://www.jpl.nasa.gov/images/kepler/20120111/pia15257-640.jpg',
+    system_name=sysname)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """TODO custom 404 error."""
-    return "Sorry, I can't find a planet at this URL.", 404
+  """TODO custom 404 error."""
+  return "Sorry, I can't find a planet at this URL.", 404
 
 
 @app.errorhandler(500)
 def application_error(e):
-    """TODO custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+  """TODO custom 500 error."""
+  return 'Sorry, unexpected error: {}'.format(e), 500
